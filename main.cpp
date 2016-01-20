7faedaa5ebf2f1a4acb317434d38259b74293102
Dylan Walseth <DylanWalseth@gmail.com>
2016-01-19 21:48:23 -0600

    Cleanup

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+/**
+ * @author Dylan Walseth(walsethd)
+ * Build with args -std=c++11 -lpthread
+ **/
+
 #include <iostream>
 #include <time.h>
 #include <pthread.h>
@@ -7,40 +12,52 @@
 #include <signal.h>
 #include <unistd.h>
 
-#define QUEUE_SIZE 20 /* total number of slots */
+#define QUEUE_SIZE 20 /* total number of slots in the queue */
 
-/**
- * @author Dylan Walseth(walsethd)
- **/
 using namespace std;
 
+// Which operation type will be performed
 typedef enum {
     ADD,
     SUB,
     MUL,
     DIV
 } operation_type;
+// Pretty name
 const char *operation_names[] = {"ADD", "SUB", "MUL", "DIV"};
 
+// Operation object/struct
 typedef struct operation{
     int a;
     int b;
     operation_type op_type;
 } operation;
 
+// Operations managers
 int total_operations_produced = 0;
 int total_operations_consumed = 0;
 int total_operations;
+
+// Queue managers
 operation operations[QUEUE_SIZE];
 int operations_in_queue = 0;
 int current_write_pos = 0;
 int current_read_pos = 0;
+
+// Expected number of operations to finish
 int num_operations;
+
+// Flag to notify consumers to stop eating so much and loss some weight
 bool flag_consumers = false;
 
+// Mutex setup
 pthread_mutex_t queue_mutex;
 pthread_cond_t cond;
 
+/**
+ * Takes in an operation struct and returns the result based on
+ * which operation_type
+ */
 int perform_operation(operation op){
     int result = 0;
     switch (op.op_type){
@@ -60,6 +77,10 @@ int perform_operation(operation op){
     return result;
 }
 
+/**
+ * Creates and moves operations into queue to be consumed by consumer
+ * Provided with a seed to ensure random numbers
+ */
 void *producer(void *seed){
     // Need to create a new seed for each thread or else they will all just produce the same operations...
     srand((unsigned int)seed);
@@ -90,6 +111,10 @@ void *producer(void *seed){
     return 0;
 };
 
+/**
+ * Consumes operations and executes operations in the queue
+ * Takes in param because my IDE was complaining that is was expecting a param
+ */
 void *consumer(void *nothing){
     int consumed = 0;
 
@@ -112,26 +137,32 @@ void *consumer(void *nothing){
     return 0;
 }
 
+/**
+ * Handles SIGUSR1 signal bu printing some info about the current queue
+ */
 void sigusr1_handler(int signum){
     printf("Operations produced: %d\n", total_operations_produced);
     printf("Operations in queue: %d\n", operations_in_queue);
 }
 
+/**
+ * Handles SIGUSR2 signal bu printing some info about the current queue
+ */
 void sigusr2_handler(int signum){
     printf("Operations consumed: %d\n", total_operations_consumed);
     printf("Operations in queue: %d\n", operations_in_queue);
 }
 
 int main(int argc, char *argv[]) {
-    signal(SIGUSR1, sigusr1_handler);
-    signal(SIGUSR2, sigusr2_handler);
-    // Going to assume input is always correct number of arguments and hope the user
-    // doesn't feel like breaking my program today
     if(argc != 4) {
         printf("Incorrect number of arguments, Expected: 3, Actual: %d\n", argc-1);
         return 1;
     }
 
+    signal(SIGUSR1, sigusr1_handler);
+    signal(SIGUSR2, sigusr2_handler);
+
+    // Generate seed based on time
     srand(static_cast<unsigned int>(time(NULL)));
 
     pthread_mutex_init(&queue_mutex, NULL);
@@ -145,6 +176,10 @@ int main(int argc, char *argv[]) {
     pthread_t producers[num_producers];
     pthread_t consumers[num_consumers];
     for(int i = 0; i < num_producers; i++){
+        /* Give each producer it's own seedling (If I tried to generate a new seed in each of the producers,
+         * they are all created at nearly the same time and their seeds are the same, which is boring because they
+         * will generate identical operations then.
+         */
         int seed = rand();
         pthread_create(&producers[i], NULL, producer, (void*) seed);
     }
@@ -168,4 +203,3 @@ int main(int argc, char *argv[]) {
     pthread_exit(NULL);
     return 0;
 }
-
